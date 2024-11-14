5f77ceca575b1d4bb97dd64a31eaa7b4e4d7a0e8
Muzammil <md.muzammil939@gmail.com>
2024-11-14 11:53:02 +0530

    push

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:kotlin_test/main.dart';
+import 'package:talk2purohith/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
